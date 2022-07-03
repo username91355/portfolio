7512ad7015d5dfebd352e789980be6054e3546c6
username91355 <personalmailD13@gmail.com>
2022-07-03 19:45:58 +0300

    fix arrows in projects slider

--- src/components/TransparentButton/TransparentButton.tsx
+++ src/components/TransparentButton/TransparentButton.tsx
@@ -1,5 +1,6 @@
 import React, {ButtonHTMLAttributes, DetailedHTMLProps } from 'react';
 import styles from './TransparentButton.module.scss';
+import cn from 'classnames';
 import {IoChevronBack, IoChevronForward} from 'react-icons/io5';
 
 interface IProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
@@ -13,9 +14,9 @@ const TransparentButton: React.FC<IProps> = props => {
 
 	switch (direction) {
 		case 'back':
-			return <button {...restProps} className={styles.button}><IoChevronBack size={'48px'}/></button>;
+			return <button {...restProps} className={cn(styles.button, styles.backButton)}><IoChevronBack size={'48px'}/></button>;
 		case 'forward':
-			return <button {...restProps} className={styles.button}><IoChevronForward size={'48px'}/></button>;
+			return <button {...restProps} className={cn(styles.button, styles.forwardButton)}><IoChevronForward size={'48px'}/></button>;
 		default:
 			return <button {...restProps} className={styles.button}>{title}</button>;
 	}
